306cce027b80e3e189c37ed7d060df4ecd5e91dc
Skye Macdonald <skyem@hotmail.co.uk>
2011-12-31 18:44:06 +0000

    Version 0.0.0.1 Developer
    Signed-off-by: Skye Macdonald <skyem@hotmail.co.uk>

---
+++ Commands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace XCL_
+{
+    public static class Commands
+    {
+        public static void messagebox(string[] message){
+            if (message[0] == "|") { if (Functions.argumentgetarar(message)[0] == "input") { message = Commands.input(Functions.argumentgetarstr(Functions.argumentgetarar(message))).Split(' '); } }
+            MessageBox.Show(string.Join(" ",message));
+
+        }
+
+        public static string input(string inputstr)
+        {
+            input inputd = new input(); inputd.Text = inputstr; inputd.ShowDialog();
+            string inputo = inputd.inputs;
+            inputd = null;
+            return inputo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace XCL_
+{
+    public static class values
+    {
+        // sets up read only values
+        public const string version = "0.0.0.1 Developer";
+        public const string numver = "0001";
+        public const string name = "XCL+";
+        public const string nameversion = name + " " + version;
+        public const string company = "INTERTECK";
+        public const string programmer = "Skye Macdonald";
+        public const string tester = "Charity Hall";
+        public const string progcomp = programmer + " Of " + company;
+        public const string testcomp = tester + " Of " + company;
+        // Sets up editable values
+        public static string prompt { get; set; }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string arg = string.Join(" ", args);
+            if (arg.Length > 0)
+            {
+                xclrun(arg);
+            }
+            else { MessageBox.Show("No XCL+ Files to Run!", "XCL+");  }
+        }
+
+        public static void xclrun(string filename)
+        {
+            string line = "";
+            TextReader text = new StreamReader(filename);
+            while (line != null)
+            {
+                // reads the file
+                line = text.ReadLine();
+                // ends if line is null
+                if (line == null) { text.Close(); return; }
+                // makes new lowercase string
+                string inputlr = line.ToLower();
+                // converts string "inputlr" to an array of strings called "inputlrar" 
+                string[] inputlrar = inputlr.Split(new char[] { ' ' });
+                // converts string "line" to an array of strings called "inputar" 
+                string[] inputar = line.Split(new char[] { ' ' });
+                // exits in string is "exit"
+                if (inputlrar[0] == "exit") { return; }
+                // starts the command find system & see if the command was (in)valid and prints error message if appropriate
+                if (cmdrun(inputlrar, inputar, line) == true) { MessageBox.Show("Bad Command: " + inputar[0], "XCL+"); }
+            }
+        }
+
+        public static Boolean cmdrun(string[] inputlr, string[] input, string inputstr)
+        {
+            // the messagebox command
+            if (inputlr[0] == "messagebox") { Commands.messagebox(Functions.argumentgetarar(input)); return false; }
+            return true;
+        }
+    }
+}
